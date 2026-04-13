5ac983c4ccda4004ba81473390e809b9912aa491
KsenijaD-design <dksenia073@gmail.com>
2026-04-13 12:07:47 +0300

    Lesson push

---
+++ Sky game/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,34 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControler : MonoBehaviour
+{
+    private InputAction move;
+    [SerializeField] private float RotationSpeed, moveSpeed;
+    private Rigidbody rb;
+    
+        
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        move = InputSystem.actions.FindAction("Player/Move");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        Vector2 moveVector = move.ReadValue<Vector2>();
+        float slopeAngle = Mathf.Abs(transform.localEulerAngles.y - 180);
+        float speedMultiplayer = Mathf.Cos(Mathf.Deg2Rad * slopeAngle);
+        rb.AddForce(transform.forward * moveSpeed * speedMultiplayer * Time.fixedDeltaTime);
+        transform.Rotate(0, moveVector.x * RotationSpeed * Time.fixedDeltaTime, 0);
+
+        
+        
+
+       
+    }
+}
