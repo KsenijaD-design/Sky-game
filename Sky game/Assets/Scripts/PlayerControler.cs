46bf38cca861b012379c7a5ad9916f0c38d5a36c
KsenijaD-design <dksenia073@gmail.com>
2026-04-27 09:37:49 +0300

    Code

--- Sky game/Assets/Scripts/PlayerControler.cs
+++ Sky game/Assets/Scripts/PlayerControler.cs
@@ -6,6 +6,8 @@ public class PlayerControler : MonoBehaviour
 {
     private InputAction move;
     [SerializeField] private float RotationSpeed, moveSpeed;
+    [SerializeField] private bool isGrounded = true;
+    [SerializeField] private LayerMask groundLayer;
     private Rigidbody rb;
     
         
@@ -19,16 +21,16 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        isGrounded = Physics.Linecast(transform.position, transform.position - transform.up * 2, groundLayer);
+        Debug.DrawRay(transform.position, transform.position - transform.up * 2);
+        if (isGrounded)
+        {
+            Vector2 moveVector = move.ReadValue<Vector2>();
+            float slopeAngle = Mathf.Abs(transform.localEulerAngles.y - 180);
+            float speedMultiplayer = Mathf.Cos(Mathf.Deg2Rad * slopeAngle);
+            rb.AddForce(transform.forward * moveSpeed * speedMultiplayer * Time.fixedDeltaTime);
+            transform.Rotate(0, moveVector.x * RotationSpeed * Time.fixedDeltaTime, 0);
+        }
         
-        Vector2 moveVector = move.ReadValue<Vector2>();
-        float slopeAngle = Mathf.Abs(transform.localEulerAngles.y - 180);
-        float speedMultiplayer = Mathf.Cos(Mathf.Deg2Rad * slopeAngle);
-        rb.AddForce(transform.forward * moveSpeed * speedMultiplayer * Time.fixedDeltaTime);
-        transform.Rotate(0, moveVector.x * RotationSpeed * Time.fixedDeltaTime, 0);
-
-        
-        
-
-       
     }
 }
